8a7cfabf3cc943bc3025fc41d6f6e66dd1828f6d
Dmitriy Martynov <dm346@mail.ru>
2023-11-14 13:17:16 +0300

    /

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,8 @@
 <template>
     <div style="max-width:600px; margin-left: auto; margin-right: auto;">
-<img src="/public/photo.png">
-</div>
+        <img src="/public/photo.png" style="float: left;">
+        <b style="font-size: 30px; color: #4c4f69; text-decoration: #d20f39 underline; padding-top: 40px; margin-left: 40px;">Hey, I'm Dima</b>
+    </div>
 </template>
 
 <script setup>
