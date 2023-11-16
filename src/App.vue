eee0583b6f3db4c41ec683ee7cdbcbaeedb25234
Dmitriy Martynov <dm346@mail.ru>
2023-11-16 12:14:49 +0300

    /

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <header style="height: 10px; max-width: 970px; margin-left: auto; margin-right: auto;">
     <img src="./assets/biglogo.png" style="float: left;">
-    <RouterLink style="float: right; font-size: 20px; margin-top: 3px;" :to="{ name: 'projects' }"><b>Projects</b></RouterLink>
+    <RouterLink style="float: right; font-size: 20px; margin-top: 3px;" :to="{ name: 'projects' }"><b>Projects</b>
+    </RouterLink>
   </header>
   <div style="background-color: #e5e7eb;max-width: 10000px; height: 1px; margin-left: -100px; margin-top: 30px;"></div>
 
@@ -10,6 +11,31 @@
   <div style="max-width: 970px;height: 300px; margin-left: auto; margin-right: auto;">
     <router-view>
     </router-view>
+  </div>
+
+
+
+
+
+  <div style=" margin-left: auto; margin-right: auto;max-width:600px; margin-top: 160px; text-align: center;">
+    <a href="https://github.com/Dexone"><img src="/public/github.png" width="35px" height="30px"></a>
+    <a href="https://vk.com/id291002761" style="margin-left: 20px;"><img src="/public/vk.png" width="35px"
+        height="30px"></a>
+    <a href="https://t.me/real_martynov" style="margin-left: 20px;"><img src="/public/telegram.png" width="35px"
+        height="30px"></a>
+  </div>
+
+
+  <div
+    style=" margin-left: auto; margin-right: auto;max-width:600px; font-size: 20px;margin-top: 10px; text-align: center;">
+    Made with <a style="color: red">❤</a> using <a href="https://vuejs.org/">Vue.js</a>, hosted on <a
+      href="https://pages.github.com/">Github Pages</a>.
+  </div>
+
+
+  <div
+    style=" margin-left: auto; margin-right: auto;max-width:600px; font-size: 20px;margin-top: 10px; text-align: center;">
+    ©2023 — Dima 'Dexone' Martynov.
   </div>
 </template>
 
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,5 +1,21 @@
 <template>
-projects
+    <b style="padding-top: 17px; font-size: 35px;  display: flex; text-decoration: #d20f39 underline;">Projects</b>
+    <div style=" font-size: 20px;margin-top: 10px;margin-bottom: 30px;">
+        You will find here the projects I work(ed) on and their GitHub repo.
+    </div>
+    <b style="font-size: 25px;">M.Weather</b>
+
+    <div style="margin-top: 10px; margin-bottom: 10px;"><img src="/public/referens.png" style=" width: 250px;"></div>
+
+    <div style=" font-size: 20px; margin-bottom: 10px;">Weather forecast in different cities</div>
+
+    <div>
+        <a style="margin-right: 5px;" href="https://dexone.github.io/M.Weather/"><img src="/public/demolink.png"
+                width="70px"></a>
+        <a href="https://github.com/Dexone/M.Weather"><img src="/public/demogit.png" width="70px"></a>
+    </div>
+
+    <div style="background-color: #e5e7eb;max-width: 10000px; height: 1px; margin-top: 30px;"></div>
 </template>
 
 <script setup>
